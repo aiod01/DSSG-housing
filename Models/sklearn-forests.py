e3347f339bc5b4c1d569362bb80a9ceced41bca1
Jocelyn Lee <jocelyn.lee@alumni.ubc.ca>
2018-08-13 11:40:55 -0700

    2 feature random forest

--- Models/sklearn-forests.py
+++ Models/sklearn-forests.py
@@ -1,5 +1,6 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import cross_val_score
 import Common.nlp_utils as nutils
 import pandas as pd
 import numpy as np
@@ -20,29 +21,38 @@ f = "../results/Standardized_Deduped_Datasets/1000samples_20180810-JL_partial_la
 df = pd.read_csv(f)
 vec = CountVectorizer(stop_words=None)
 
+
+# Missing values
+df['sqft'] = df['sqft'].dropna().apply(lambda x: float(re.sub("\D", "", x)))
+
 df['lat'].fillna(-1, inplace=True)
 df['long'].fillna(-1, inplace=True)
 df['price'].fillna(-1, inplace=True)
+df['sqft'].fillna(-1, inplace=True)
 df['description'].fillna("", inplace=True)
 df['title'].fillna("", inplace=True)
 
-desc_corpus = vec.fit_transform(df['description'])
-desc = vec.transform(df['description'])
+train = df.iloc[:99]
+test = df.iloc[99:]
 
-title_corpus = vec.fit_transform(df['title'])
-title = vec.transform(df['title'])
+# Bag of words
+desc_corpus = vec.fit_transform(train['description'])
+
+title_corpus = vec.fit_transform(train['title'])
+title_corpus = title_corpus.toarray()
 # df['desc_bag'] = df.apply(bag_words, args=("desc",), axis=1)
 # print(df['desc_bag'].head())
 # df['title_bag'] = df.apply(bag_words, args=("title_no_stop",), axis=1)
 
 
-train = df.iloc[:99]
-test = df.iloc[99:]
-features = ['lat', 'long', 'price']
+features = ['price', 'sqft']
 
 clf = RandomForestClassifier(n_jobs=2, random_state=1234)
 clf.fit(train[features], train['Label_0-entire_1-part'])
 prediction = clf.predict(test[features])
-print(prediction)
 test['predicted'] = prediction
-test.to_csv("../results/Standardized_Deduped_Datasets/1000samples_20180810_pred.csv")
+
+#test.to_csv("../results/Standardized_Deduped_Datasets/1000samples_20180810_pred.csv")
+
+scores = cross_val_score(clf, train[features], train['Label_0-entire_1-part'], cv=10)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
