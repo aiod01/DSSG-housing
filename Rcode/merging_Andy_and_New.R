853dbecb8963b25eba007868ae5c40f5561b6bc3
hyeongcheol park <hyeongcheol.park@stat.ubc.ca>
2018-08-15 14:36:33 -0700

    MICE done

--- Rcode/merging_Andy_and_New.R
+++ Rcode/merging_Andy_and_New.R
@@ -1,14 +1,16 @@
 if (!require(dplyr)) install.packages("dplyr")
 library("dplyr")
 
-
 s<-getwd()
-datapath1<-paste(s,"/Desktop/DSSG/gitscripper/DSSG-2018_Housing/results/Standardized_Deduped_Datasets/meeting_sample.csv",sep = "")
-datapath2<-paste(s,"/Desktop/DSSG/gitscripper/DSSG-2018_Housing/results/Standardized_Deduped_Datasets/1000samples_20180810.csv",sep = "")
+datapath1<-paste(s,"/Desktop/DSSG/gitscripper/DSSG-2018_Housing/results/Standardized_Deduped_Datasets/1000samples_20180813_merged.csv",sep = "")
+datapath2<-paste(s,"/Desktop/DSSG/gitscripper/DSSG-2018_Housing/results/Standardized_Deduped_Datasets/1000samples_20180810-JL_partial_labels.csv",sep = "")
 
-dat1 <- read.csv(file=datapath1,header=T,stringsAsFactors = FALSE,na.strings = c("","NA")) %>% select(Category,url)
+dat1 <- read.csv(file=datapath1,header=T,stringsAsFactors = FALSE,na.strings = c("","NA")) 
+dat1$X <- NULL
+dat1$X.1 <- NULL
 dat2 <- read.csv(file=datapath2,header=T,stringsAsFactors = FALSE,na.strings = c("","NA"))
+dat2 <- dat2 %>% select(url,Label_0.entire_1.part,Label_0.entire_1.suite_2.room)
 
-total <- left_join(dat2,dat1,by="url")
+total <- left_join(dat1,dat2,by="url")
 
 write.csv(total, file = "/Users/hyeongcheolpark/Desktop/DSSG/gitscripper/DSSG-2018_Housing/results/Standardized_Deduped_Datasets/1000samples_20180810_merged.csv")
