49456f9570770322f7ca643002e58e712e3f12b3
zjiang <z.jiang@alumni.ubc.ca>
2018-08-24 11:26:37 -0700

    checking proportion of Naive Bayes model results

--- Models/Naive Bayes/NBModel.py
+++ Models/Naive Bayes/NBModel.py
@@ -5,17 +5,22 @@ from sklearn.naive_bayes import GaussianNB, BernoulliNB, MultinomialNB
 from sklearn.metrics import accuracy_score
 
 
-#Pre-processing the dataset
+# Pre-processing the dataset
 def label_housing_type(row):
     cat = row['Category']
     if cat >= 0 and cat < 3: return 'A'
     if cat >= 3 and cat < 4: return 'B'
-    if cat == 4: return 'C'
-    else: return cat
+    if cat == 4:
+        return 'C'
+    else:
+        return cat
+
 
 def clean_na(grid):
-    if not grid >= 0: return 0
-    else: return grid
+    if not grid >= 0:
+        return 0
+    else:
+        return grid
 
 
 csv_data = pd.read_csv('Imputated_data_m_1_20180822.csv')
@@ -32,22 +37,19 @@ df['label'] = df.apply(lambda row: label_housing_type(row), axis=1)
 
 data = df[['price', 'rooms', 'sqft', 'label']]
 
-
 # print(df.loc[0:20][['price', 'rooms', 'sqft', 'label']])
 # print(data[0:20])
 
 X = data[['price', 'rooms', 'sqft']]
 y = data['label']
 
-#Checking the proportion of each label
+# Checking the proportion of each label
 summary = data.groupby('label')['price'].count().reset_index()
-summary['proportion'] = summary['price']/summary['price'].sum()
-summary.rename(columns = {'price' : 'counts'}, inplace = True)
+summary['proportion'] = summary['price'] / summary['price'].sum()
+summary.rename(columns={'price': 'counts'}, inplace=True)
 print(summary)
 
-
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state= None)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=None)
 
 model = GaussianNB()
 model.fit(X_train, y_train)
@@ -55,10 +57,19 @@ y_pred = model.predict(X_test)
 
 accuracy = accuracy_score(y_test, y_pred)
 
+print('Score is : ' + str(accuracy))
 
-print('Score is : '+ str(accuracy))
+
+y_pred = y_pred.tolist()
+
+unique, counts = np.unique(y_pred, return_counts=True)
+prop = dict(zip(unique, counts))
+
+print(prop)
+
+
+print(type(y_pred))
+
 
 
 # print(df.dtypes)
-
-
