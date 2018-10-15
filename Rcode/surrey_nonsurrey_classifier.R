7d11f0075c7f0ae59f82452a8001049eaed65e05
hyeongcheol park <hyeongcheol.park@stat.ubc.ca>
2018-10-14 22:42:23 -0700

    non surrey classifier code update

--- Rcode/surrey_nonsurrey_classifier.R
+++ Rcode/surrey_nonsurrey_classifier.R
@@ -2,16 +2,20 @@ if (!require(tidyverse)) install.packages("tidyverse")
 library("tidyverse")
 
 
-#Load data set
+##Load data set
 s<-getwd()
-substr(s, 1, nchar(s)-5)
-datapath<-paste(substr(s, 1, nchar(s)-5),"/results/Standardized_Deduped_Datasets/July_Clean_20180718.csv",sep = "")
+#For the pipeline
+#datapath<-paste(s,"/Desktop/DSSG/gitscripper/DSSG-2018_Housing/results/Standardized_Deduped_Datasets/July_Clean_20180718_withNonSurrey.csv",sep = "")
+#For Aug, Sep data set.
+datapath<-paste(s,"/Desktop/Aug_Sep_clean20181010.csv",sep = "")
+
 #If you cannot load the raw dataset, you need to set it by yourself by matching the csv file name.
 result <- read.csv(file=datapath,header=T,stringsAsFactors = FALSE)
+result$Unnamed..0 <- NULL #Wierd column deleted
 
 #Arrangnig the dataset by title.
 result<- result %>% 
-  arrange(description)
+  arrange(title)
 
 #Adding index cuz the rowname is not functioning very well.
 result$ID <- seq.int(nrow(result))
@@ -20,28 +24,35 @@ result$ID <- seq.int(nrow(result))
 #But there are some empty title and description, so I am deleting empty titles.
 result <- result[!(result$title==""),]
 
-
 #Let's make a function for the data spliting.
+#This function gives us data points which has 'name' value on its location, title and description
 a.regional.data <- function(name){
-  
+  #in location, title and description, catch if a value has 'name' string on it.
   surrey_location_list <- gregexpr(pattern=name, result$location)
   surrey_title_list <- gregexpr(pattern=name, result$title)
-  index_surrey <- c()
-  index_surrey_title <- c()
+  surrey_description_list <- gregexpr(pattern=name, result$description)
+  #empty vector to save the index of detected data
+  index_surrey_location <- index_surrey_description <- index_surrey_title<-  c()
+  # for each variable (location, title, description), save the index into the empty vector for each.
   for (i in 1:nrow(result)) {
     if(surrey_location_list[[i]][1]!=-1)
-    {index_surrey <- c(index_surrey,i)}
+    {index_surrey_location <- c(index_surrey_location,i)}
     if(surrey_title_list[[i]][1]!=-1)
     {index_surrey_title <- c(index_surrey_title,i)}
+    if(surrey_description_list[[i]][1]!=-1)
+    {index_surrey_description <- c(index_surrey_description,i)}
   }
-  index <- c(index_surrey,index_surrey_title)
+  #into one bag of vector put all the indexes. 
+  index <- c(index_surrey_location,index_surrey_title,index_surrey_description)
+  #delete duplicated index
   index <- index[!duplicated(index)]
+  #give me the detected data points.
+  index <- sort(index)
   return(result[index,])
-}
-# a.regional.data function!
+}# a.regional.data function: detect a single string from location, title and description.
 
 #This is to let the function deal with a vector with multiple region names.
-subset.data <- function(x) {
+subset.data <- function(x) {#'x' is a vector with regional names 
   l.result<- lapply(x, a.regional.data)
   temp1 <- l.result[[1]]
   for (i in 1:(length(l.result)-1)) {
@@ -73,6 +84,8 @@ classifier <- function(vector1,vector2)
   #complementary subset
   index1 <- dt1$ID
   index2 <- dt2$ID
+  only_nonsurrey <- dt2 %>% 
+    filter(!(ID %in% dt1$ID))
   complementary.index <- result$ID[-sort(combine(index1, index2))[!duplicated(sort(combine(index1, index2)))]]
   complementary.data <- result[complementary.index,]
   #print(deparse(substitute(vector1))) When you debuging, run inside of a mold, line by line, print things on each step.
@@ -80,6 +93,7 @@ classifier <- function(vector1,vector2)
   x[[deparse(substitute(vector2))]]=dt2
   x[["complementary.data"]] = complementary.data
   x[["confusing.data"]] = confusing.data
+  x[["only_nonsurrey"]] = only_nonsurrey
   return(x)
 }
 
@@ -87,14 +101,28 @@ classifier <- function(vector1,vector2)
 #Surrey
 surrey.region <- c("halley","uildford","leetwood","estminster", "ewton","loverdale","urrey","ity centre")
 
-non.surrey.region <- c("hite rock","ission","sawwassen","elta")
+non.surrey.region <- c("hite rock","ission","sawwassen","elta","urnaby","angley","oquitlam","DELTA","WHITE ROCK","BURNABY")
 # non.surrey.data<- subset.data(non.surrey.region)
 
 test <- classifier(surrey.region, non.surrey.region)
 
-surrey.region.data <- rbind(test[[1]],test[[3]],test[[4]])
-non.surrey.region.data <- test[[2]]
+nonsurrey_ID <- test[["only_nonsurrey"]]$ID
+surrey_conservative <- result %>% filter(!(ID%in%nonsurrey_ID))
 
+#To make automatic cleaned name for the sake of pipeline.
+strn <- date()
+strn
+year<- substr(strn, 21, 24)
+date <- substr(strn, 9, 10)
+strn_sys <- Sys.Date()
+month <- substr(strn_sys,6,7)
+month
+suffix <- paste(year,month,date,".csv", sep = "", collapse = NULL)
 
-filename<-paste(substr(s, 1, nchar(s)-5),"/results/Standardized_Deduped_Datasets/July_Clean_20180719.csv",sep = "")
-write.csv(surrey.region.data,file=filename)
+#the result of automatic name 
+name <- paste("Aug_Sep_clean",suffix,sep="")
+setwd("/Users/hyeongcheolpark/Desktop/DSSG/gitscripper/DSSG-2018_Housing/results/Standardized_Deduped_Datasets")
+filename<-paste(getwd(),name,sep = "/")
+
+#save csv file.
+write.csv(surrey_conservative,file=filename)
